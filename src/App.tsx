6c7fc7df1918215bceb6dbe63eebe7372e9ec9ab
wahyuuuwid <idwahyu58@gmail.com>
2026-03-25 08:56:27 +0700

    edit: majority route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NotFound from "./pages/NotFound";
+import Tentang from "./pages/Tentang";
+import Kontak from "./pages/Kontak";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsConditions from "./pages/TermsConditions";
+import PusatBantuan from "./pages/HelpCenter";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/roadmap/:id" element={<Detail />} />
+      <Route path="/tentang" element={<Tentang />} />
+      <Route path="/kontak" element={<Kontak />} />
+
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-and-conditions" element={<TermsConditions />} />
+      <Route path="/pusat-bantuan" element={<PusatBantuan />} />
 
       {/* 404 not found */}
       <Route path="*" element={<NotFound />} />
