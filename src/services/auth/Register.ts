1b70a3ee5b20da10773eff891894e9c9e233a823
wahyuuuwid <idwahyu58@gmail.com>
2026-03-29 11:33:21 +0700

    edit: url

--- src/services/auth/Register.ts
+++ src/services/auth/Register.ts
@@ -2,7 +2,7 @@ import api from "../api";
 
 export const RegisterService = async (data: any) => {
     try {
-        const response = await api.post("/register", data);
+        const response = await api.post("/auth/register", data);
         return response.data;
     } catch (error: any) {
         console.error("API Error:", error.response?.data || error.message);
