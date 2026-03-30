2ab3da2deeb40daaddc223897dd0b3dd32a830ef
wahyuuuwid <idwahyu58@gmail.com>
2026-03-30 11:22:56 +0700

    fix: main.tsx path for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Navbar } from './components/layouts/Navbar.tsx'
-import { Footer } from './components/layouts/Footer.tsx'
+import { Navbar } from './components/layouts/Navbar'
+import { Footer } from './components/layouts/Footer'
 import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from './components/layouts/ScrollToTop.tsx'
+import ScrollToTop from './components/layouts/ScrollToTop'
 import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
